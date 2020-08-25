697473a83c21b0db00f709ed9c8294f4cc27eacd
Яков Гущин <yasha98@list.ru>
2020-08-25 18:51:44 +0700

    feat(sidebar): fix sidebar item unit test

--- src/packages/layout/src/sidebar/__tests__/sidebar-item.directive.spec.ts
+++ src/packages/layout/src/sidebar/__tests__/sidebar-item.directive.spec.ts
@@ -1,23 +1,42 @@
 import {ESidebarItemDirective} from '../sidebar-item.directive';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {Component, ElementRef, NO_ERRORS_SCHEMA, Renderer2, TemplateRef, ViewContainerRef} from '@angular/core';
+
+@Component({template: `<div *eSidebarItem="let activeClass = activeClass; tooltip: 'tooltip'"></div>`})
+class TestSidebarItemComponent {
+}
 
 describe('SidebarItemDirective', (): void => {
-    let directive: ESidebarItemDirective;
-    let fixture: ComponentFixture<ESidebarItemDirective>;
+    let fixture: ComponentFixture<TestSidebarItemComponent>;
+
+    // tslint:disable-next-line:prefer-const
+    let el: jasmine.SpyObj<ElementRef>;
+    // tslint:disable-next-line:prefer-const
+    let renderer: jasmine.SpyObj<Renderer2>;
+    // tslint:disable-next-line:prefer-const
+    let templateRef: jasmine.SpyObj<TemplateRef<any>>;
+    // tslint:disable-next-line:prefer-const
+    let vcRef: jasmine.SpyObj<ViewContainerRef>;
 
     beforeEach(async((): void => {
         TestBed.configureTestingModule({
-            declarations: [ESidebarItemDirective]
+            declarations: [ESidebarItemDirective, ESidebarItemDirective],
+            schemas: [ NO_ERRORS_SCHEMA ]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(TestSidebarItemComponent);
     }));
 
-    beforeEach((): void => {
-        fixture = TestBed.createComponent(ESidebarItemDirective);
-        directive = fixture.componentInstance;
-        fixture.detectChanges();
-    });
-
     it('should create an instance', (): void => {
+        const directive: ESidebarItemDirective = new ESidebarItemDirective(
+            el,
+            renderer,
+            templateRef,
+            vcRef
+        );
+
+        fixture.detectChanges();
+
         expect(directive).toBeTruthy();
     });
 });
